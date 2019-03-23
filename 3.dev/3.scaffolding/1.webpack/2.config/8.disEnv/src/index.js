9dbfe6b856e61534857f2a8e238a45827d9aa42c
huipaodejitui <920210993@qq.com>
2019-03-23 18:56:36 +0800

    'webpack区分不同的环境'

---
+++ 3.dev/3.scaffolding/1.webpack/2.config/8.disEnv/src/index.js
@@ -0,0 +1,9 @@
+let url = "";
+if (DEV==='dev') {
+  url = "http:localhost:3000";
+} else {
+  url = "http:zhoubichuan.com";
+}
+console.log(url);
+console.log(typeof FLAG);
+console.log(EXPORESSION);
