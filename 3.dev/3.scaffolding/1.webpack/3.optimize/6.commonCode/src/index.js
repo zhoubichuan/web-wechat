b1a4dd0fa811b7170f6fd643163fd4ef12c867b6
huipaodejitui <920210993@qq.com>
2019-03-28 22:59:08 +0800

    webpack-抽离公共代码

---
+++ 3.dev/3.scaffolding/1.webpack/3.optimize/6.commonCode/src/index.js
@@ -0,0 +1,6 @@
+import a from "./a.js";
+import b from "./b.js";
+import $ from "jquery";
+
+console.log(a, b, "index.js");
+console.log($);
---
+++ 3.dev/3.scaffolding/1.webpack/3.optimize/6.commonCode/src/other.js
@@ -0,0 +1,6 @@
+import a from "./a.js";
+import b from "./b.js";
+import $ from "jquery";
+
+console.log(a, b, "other.js");
+console.log($);
