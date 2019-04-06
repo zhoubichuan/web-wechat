c5a0e65cb19596e3e99653fa43a52b8a0784c7fd
huipaodejitui <920210993@qq.com>
2019-04-06 17:10:55 +0800

    webpack-内联webpack插件

---
+++ 3.dev/3.scaffolding/1.webpack/7.plugin/4.inline/src/index.js
@@ -0,0 +1,2 @@
+import './index.css'
+console.log(1111111111)
--- 3.dev/3.scaffolding/1.webpack/7.plugin/4.inline/webpack.config.js
+++ 3.dev/3.scaffolding/1.webpack/7.plugin/4.inline/webpack.config.js
@@ -1,6 +1,8 @@
 let path = require("path");
 let HtmlWebpackPlugin = require("html-webpack-plugin");
 let FileListPlugin = require("./plugins/FileListPlugin");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let InlineSourcePlugin = require("./plugins/InlineSourcePlugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -8,12 +10,26 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "main.css"
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html"
     }),
     new FileListPlugin({
       filename: "list.md"
+    }),
+    new InlineSourcePlugin({
+      match: /\.js|.css/
     })
   ]
 };
