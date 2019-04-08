f81635eba06af9a011c80cd57a2e23279a0122ca
huipaodejitui <920210993@qq.com>
2019-04-08 22:38:07 +0800

    es6-let

--- 2.stand/1.ECMA/4.es6/1.let&const/index.js
+++ 2.stand/1.ECMA/4.es6/1.let&const/index.js
@@ -1,6 +1,28 @@
 //var 不支持封闭作用域,会声明到全局作用域上
 //1.函数作用域名
 //2.全局作用域
-for(var i=0;i<3;i++){
+// (function() {
+//   for (var i = 0; i < 3; i++) {
+//     console.log(i);
+//   }
+// })();
+// console.log(i);
+// console.log(window.i);
 
+// for (var i = 0; i < 3; i++) {
+//   (function(i){
+//     setTimeout(function() {
+//         console.log(i);
+//       }, 1000);
+//   })(i)
+// }
+
+//let支持块级作用域声明的变量只会声明在当前的作用域内
+for (let i = 0; i < 3; i++) {
+  //let i=0
+  setTimeout(function() {
+    console.log(i);
+  }, 1000);
 }
+console.log(i)
+//let 可以解决作用域污染的问题 和局部作用域的问题
