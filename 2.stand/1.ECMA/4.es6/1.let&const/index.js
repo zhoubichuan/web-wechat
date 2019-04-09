68569b32de18efa297f5102a212acc11edcb21c1
huipaodejitui <920210993@qq.com>
2019-04-09 22:48:07 +0800

    es6-let

--- 2.stand/1.ECMA/4.es6/1.let&const/index.js
+++ 2.stand/1.ECMA/4.es6/1.let&const/index.js
@@ -18,11 +18,32 @@
 // }
 
 //let支持块级作用域声明的变量只会声明在当前的作用域内
-for (let i = 0; i < 3; i++) {
-  //let i=0
-  setTimeout(function() {
-    console.log(i);
-  }, 1000);
-}
-console.log(i)
+// for (let i = 0; i < 3; i++) {
+//   //let i=0
+//   setTimeout(function() {
+//     console.log(i);
+//   }, 1000);
+// }
+// console.log(i)
 //let 可以解决作用域污染的问题 和局部作用域的问题
+
+//二.在同一个作用域下可以多次声明同一个变量
+// var a =1
+// function b(){
+//   var a=2
+//   var a =4
+// }
+// b()
+
+// var a =1
+// function b(){
+//   let a=2
+//   let a =4
+// }
+// b()
+
+let a =1
+function b(){
+  let a=2
+}
+b()
