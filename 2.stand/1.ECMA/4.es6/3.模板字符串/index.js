73a34992ab895771f7fd69eb954a3c34b598ef0c
huipaodejitui <920210993@qq.com>
2019-04-15 21:45:32 +0800

    es6-模板字符串

--- 2.stand/1.ECMA/4.es6/3.模板字符串/index.js
+++ 2.stand/1.ECMA/4.es6/3.模板字符串/index.js
@@ -27,4 +27,29 @@ str2=str2.replace(/\$\{([^}]*)\}/g,function(){
 console.log(str2)
 
 //带标签的模板字符 自定义模板字符串的实现
-//hello*zfpx*今年*9*岁了
+//hello*zfpx*今年*9*岁了+
+str2=str2.replace(/\$\{([^}]*)\}/g,function(){
+    return eval(arguments[1])
+})
+console.log(str2)
+
+//字符串常用的方法
+//includes 是否包含
+let url="http://www.zhufengpeixun.cn/logo.png"
+console.log(url.includes('zhufengpeixun'))
+//startsWith 以xx开头
+console.log(url.startsWith('http://'))
+//endsWith 以xx结尾
+console.log(url.endsWith('.png'))
+//padStart padEnd 补全
+//进制转化
+setInterval(function(){
+    let date=new Date()
+    let hour=date.getHours()
+    let minutes=date.getMinutes()
+    let seconds=date.getSeconds()
+    let str=`${hour.toString().padStart(2,0)}`
+    str +=`${minutes.toString().padStart(2,0)}`
+    str +=`${seconds.toString().padStart(2,0)}`
+    console.log(str)
+},1000)
--- 2.stand/1.ECMA/4.es6/3.模板字符串/tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-let name2 ='zfpx'
-let age2=9
-let str2= 'hello${name2}今年${age2}岁了'
-str2=str2.replace(/\$\{([^}]*)\}/g,function(){
-    return eval(arguments[1])
-})
-console.log(str2)
