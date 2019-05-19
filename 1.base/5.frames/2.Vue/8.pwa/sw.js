65493e50323d20c5ad11cf9e95c7e917017331b2
huipaodejitui <920210993@qq.com>
2019-05-19 22:00:59 +0800

    pwa

--- 1.base/5.frames/2.Vue/8.pwa/sw.js
+++ 1.base/5.frames/2.Vue/8.pwa/sw.js
@@ -1,9 +1,35 @@
 const CACHE_NAME = "cache_v" + 1; //默认情况 sw文件变化后会重新注册serviceWork
 
-const CACHE_LIST = ["/", "/index.html", "/index.css", "main.js", "/api/img"];
+const CACHE_LIST = ["/", "/index.html", "/index.css", "/main.js", "/api/img"];
 
+//获取数据后进行缓存
+function fetchAddSave(request) {
+  //如果请求到了 需要更新缓存
+  return fetch(request).then(res => {
+    //res必须克隆 因为使用一次就销毁
+    let r = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(request, r));
+    return res;
+  });
+}
+//线程中 不能发ajax --> fetch fetchApi
 self.addEventListener("fetch", e => {
-  console.log(e.request.url);
+  //如果联网的话就发请求
+  if (e.request.url.includes("/api/")) {
+    return e.respondWith(
+      fetchAddSave(e.request).catch(err => {
+        //打开缓存 把缓存中匹配到的结果 返还回去
+        return caches.open(CACHE_NAME).then(cache => cache.match(e.request));
+      })
+    );
+  }
+  //用什么内容 返回当前响应
+  e.respondWith(
+    fetch(e.request).catch(err => {
+      //打开缓存 把缓存中匹配到的结果 返还回去
+      return caches.open(CACHE_NAME).then(cache => cache.match(e.request));
+    })
+  );
 });
 //缓存 需要缓存内容
 function preCache() {
@@ -16,6 +42,7 @@ self.addEventListener("install", e => {
   //如果上一个serviceWork不销毁 需要手动skipWating
   e.waitUntil(preCache().then(skipWaiting));
 });
+//激活当前serviceWork,让servicel立即生效 self.clients.claim()
 function clearCache() {
   return caches.keys().then(keys => {
     return Promise.all(
@@ -27,7 +54,7 @@ function clearCache() {
     );
   });
 }
-//激活当前serviceWork,让servicel立即生效 self.clients.claim()
+// 当前serviceWorker安装完毕后
 self.addEventListener("activate", e => {
   e.waitUntil(Promise.all([clearCache(), self.clients.claim()]));
 });
